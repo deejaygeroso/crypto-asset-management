720d6ee4946362e6d97338b58e6e324061c8d70b
Deejay Geroso <deejaygeroso@gmail.com>
2018-05-15 22:54:21 +0800

    view link

--- modules/portfolio/components/ViewLink.js
+++ modules/portfolio/components/ViewLink.js
@@ -8,77 +8,75 @@ Chart.defaults.global.responsive = true;
 
 class ViewLink extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Price USD',
+                        fill: true,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(102,255,102,0.4)',
+                        borderColor: 'rgba(75,192,192,1)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 20,
+                        data: [],
+                    },
+                ]
+            }
+        }
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        const { cryptoChartsList } = nextProps;
+        
+        this.setState({
+            data: {
+                labels: cryptoChartsList && cryptoChartsList.labels && cryptoChartsList.labels!==0 ? cryptoChartsList.labels : [],
+                datasets: [
+                    {
+                        label: 'Price USD',
+                        fill: true,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(102,255,102,0.4)',
+                        borderColor: 'rgba(75,192,192,1)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 20,
+                        data: cryptoChartsList && cryptoChartsList.price_usd && cryptoChartsList.price_usd.length!==0 ? cryptoChartsList.price_usd : [],
+                    },
+                ]
+            }
+        })
+    }
+    
+
     render() {
         const { linkList, onEdit, cryptoChartsList } = this.props;
-    
-        const data = {
-            labels: cryptoChartsList.labels,
-            datasets: [
-              {
-                label: 'Price USD',
-                fill: true,
-                lineTension: 0.1,
-                backgroundColor: 'rgba(102,255,102,0.4)',
-                borderColor: 'rgba(75,192,192,1)',
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: 'rgba(75,192,192,1)',
-                pointBackgroundColor: '#fff',
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                pointHoverBorderColor: 'rgba(220,220,220,1)',
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 20,
-                data: cryptoChartsList.price_usd,
-              },
-            //   {
-            //     label: 'Price BTC',
-            //     fill: true,
-            //     lineTension: 0.1,
-            //     backgroundColor: 'rgba(255, 0, 0, 1)',
-            //     borderColor: 'rgba(255, 0, 120, 1)',
-            //     borderCapStyle: 'butt',
-            //     borderDash: [],
-            //     borderDashOffset: 0.0,
-            //     borderJoinStyle: 'miter',
-            //     pointBorderColor: 'rgba(75,192,192,1)',
-            //     pointBackgroundColor: '#ad4949',
-            //     pointBorderWidth: 1,
-            //     pointHoverRadius: 5,
-            //     pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            //     pointHoverBorderColor: 'rgba(220,220,220,1)',
-            //     pointHoverBorderWidth: 2,
-            //     pointRadius: 2,
-            //     pointHitRadius: 20,
-            //     data: cryptoChartsList.price_btc,
-            //   },
-            //   {
-            //     label: 'Price ETH',
-            //     fill: true,
-            //     lineTension: 0.1,
-            //     backgroundColor: 'rgba(255,140,102,0.4)',
-            //     borderColor: 'rgba(75,192,192,1)',
-            //     borderCapStyle: 'butt',
-            //     borderDash: [],
-            //     borderDashOffset: 0.0,
-            //     borderJoinStyle: 'miter',
-            //     pointBorderColor: 'rgba(75,192,192,1)',
-            //     pointBackgroundColor: '#fff',
-            //     pointBorderWidth: 1,
-            //     pointHoverRadius: 5,
-            //     pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            //     pointHoverBorderColor: 'rgba(220,220,220,1)',
-            //     pointHoverBorderWidth: 2,
-            //     pointRadius: 2,
-            //     pointHitRadius: 20,
-            //     data: cryptoChartsList.price_eth,
-            //   },
-            ]
-          };
+
         return (
             <div className="flex-column align-items-center justify-content-center mt-80 ml-80 mr-80">
 
@@ -89,8 +87,12 @@ class ViewLink extends Component {
                     {/* ------------------------------*/}
                     {/* -------- Chart/Graph -------- */}
                     {/* ------------------------------*/}
+                    {/* Note: wait for cryptoChartsList data to be loaded first before actual showing of graph */}
                     <div className="col-md-12 mb-30">
-                        <Line data={data} />
+                    {
+                        cryptoChartsList && cryptoChartsList.labels && cryptoChartsList.labels.length!==0 ?
+                        <Line data={this.state.data} /> : <div />
+                    }
                     </div>
 
                     {/* ------------------------------*/}
